9de96a9e78f8b9ceae8dc7984972ce9a3f4c15cb
westtly25 <westtly25@gmail.com>
2023-07-24 15:20:43 +0300

    Add State machine, creadte mode data

---
+++ Assets/Scripts/Runtime/Data/BoardData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class BoardData
+{
+    [SerializeField, Range(4, 40)]
+    private int width;
+    [SerializeField, Range(4, 40)]
+    private int height;
+
+    [SerializeField]
+    private Cell[][] nodes;
+}
--- Assets/Scripts/Runtime/Game Board/Board.cs
+++ Assets/Scripts/Runtime/Game Board/Board.cs
@@ -4,24 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class BoardModel
-{
-    [SerializeField, Range(4, 40)]
-    private int width;
-    [SerializeField, Range(4, 40)]
-    private int height;
-
-    [SerializeField]
-    private Cell[][] nodes;
-}
-
 public class Board : MonoBehaviour
 {
     
-}
-
-[Serializable]
-public class Cell
-{
-
 }
